55f10ba4fb6951ad9c3369ab2bd0d8e126dff238
carlos.melero <cmelero96@gmail.com>
2022-01-12 17:48:48 +0100

    Extracted composable data

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -6,55 +6,16 @@
 </template>
 
 <script>
-import { onMounted, ref } from 'vue';
-import { debouncedWatch } from '@vueuse/core';
-import Fuse from 'fuse.js';
 import ALL_CITIES from '../assets/history.city.list.json';
-
-const SEARCH_OPTIONS = {
-  threshold: 0.35,
-  keys: ['city.name'],
-};
+import useSuggestions from '../composables/useSuggestions';
 
 export default {
   setup() {
-    const searchTerm = ref('');
-    const searchResults = ref([]);
-    const fuse = ref();
-
-    const getSuggestions = () => {
-      const search = fuse.value
-        .search(searchTerm.value)
-        .slice(0, 10)
-        .map((city) => {
-          let id = city.item.id;
-          if (typeof id !== 'number') {
-            id = Number(id['$numberLong']);
-          }
-
-          return {
-            id,
-            name: city.item.city.name,
-            countryCode: city.item.city.country,
-            coord: city.item.city.coord,
-          };
-        });
-      return search;
-    };
+    const { searchTerm, searchResults } = useSuggestions(ALL_CITIES);
 
     const select = (item) => {
       console.log(item);
     };
-
-    onMounted(() => (fuse.value = new Fuse(ALL_CITIES, SEARCH_OPTIONS)));
-
-    debouncedWatch(
-      searchTerm,
-      () => {
-        searchResults.value = getSuggestions(searchTerm);
-      },
-      { debounce: 150 }
-    );
 
     return { searchTerm, searchResults, select };
   },
---
+++ src/composables/useSuggestions.js
@@ -0,0 +1,48 @@
+import { debouncedWatch } from '@vueuse/core';
+import { ref, onMounted } from 'vue';
+import Fuse from 'fuse.js';
+
+const SEARCH_OPTIONS = {
+  threshold: 0.35,
+  keys: ['city.name'],
+};
+
+export default (searchValues) => {
+  // Create Fuse instance to perform fuzzy search
+  const fuse = ref();
+  onMounted(() => (fuse.value = new Fuse(searchValues, SEARCH_OPTIONS)));
+
+  // Create method to fuzzy search and handle the results
+  const searchTerm = ref('');
+  const getSuggestions = () => {
+    const search = fuse.value
+      .search(searchTerm.value)
+      .slice(0, 10)
+      .map((city) => {
+        let id = city.item.id;
+        if (typeof id !== 'number') {
+          id = Number(id['$numberLong']);
+        }
+
+        return {
+          id,
+          name: city.item.city.name,
+          countryCode: city.item.city.country,
+          coord: city.item.city.coord,
+        };
+      });
+    return search;
+  };
+
+  // Perform a new search on user input, after a short delay
+  const searchResults = ref([]);
+  debouncedWatch(
+    searchTerm,
+    () => {
+      searchResults.value = getSuggestions(searchTerm);
+    },
+    { debounce: 150 }
+  );
+
+  return { searchTerm, searchResults };
+};
