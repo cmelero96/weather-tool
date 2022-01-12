bbacd1be4fc5b1da85e4dfee1c0c41868ef10b9f
carlos.melero <cmelero96@gmail.com>
2022-01-12 12:30:47 +0100

    Refactored apiCall file to be more understandable

--- src/services/apiCall.js
+++ src/services/apiCall.js
@@ -1,63 +1,45 @@
 import API_KEY from '../assets/api-key';
 
-const callService = async (url) => {
-  const response = await fetch(`${url}&units=metric&appid=${API_KEY}`);
+export const getWeatherData = async (query) => {
+  const current = await getCurrentData(query);
 
-  if (!response.ok) {
-    console.log('error');
-  }
+  const { dt, lat, lon } = current;
 
-  const data = response.json();
+  const forecast = await getForecastData(lat, lon);
+  const historical = await getPastData(dt, lat, lon);
 
-  return data;
+  return { current, historical, forecast };
 };
 
-export const getWeatherData = async (query) => {
-  const currentResponse = await callService(
-    `https://api.openweathermap.org/data/2.5/weather?q=${query}`
-  );
+const getCurrentData = async (query) => {
+  const response = await callService(`https://api.openweathermap.org/data/2.5/weather?q=${query}`);
 
-  const { lat, lon } = currentResponse.coord;
-  const dt = currentResponse.dt;
+  const current = {
+    location: response.name,
+    lat: response.coord.lat,
+    lon: response.coord.lon,
+    description: response.weather[0].description,
+    icon: response.weather[0].icon,
+    temperature: response.main.temp,
+    minTemperature: response.main.temp_min,
+    maxTemperature: response.main.temp_max,
+    pressure: response.main.pressure,
+    humidity: response.main.humidity,
+    windSpeed: response.wind.speed,
+    sunrise: new Date(response.sys.sunrise),
+    sunset: new Date(response.sys.sunset),
+    dt: response.dt,
+  };
 
-  console.log({ lat, lon, dt });
+  return current;
+};
 
-  const forecastResponse = await callService(
+const getForecastData = async (lat, lon) => {
+  const response = await callService(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts`
   );
 
-  const dateNow = new Date(dt * 1000);
-  const oldTimestamps = [];
-  for (let i = 0; i < 5; i++) {
-    const pastDate = new Date(dateNow.getTime());
-    pastDate.setDate(dateNow.getDate() - (i + 1));
-
-    oldTimestamps.push(pastDate.getTime() / 1000);
-  }
-
-  const historicalResponse = await Promise.all(
-    oldTimestamps.map((timestamp) =>
-      callService(
-        `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${dt}`
-      )
-    )
-  );
-
-  const current = {
-    location: currentResponse.name,
-    description: currentResponse.weather[0].description,
-    icon: currentResponse.weather[0].icon,
-    temperature: currentResponse.main.temp,
-    minTemperature: currentResponse.main.temp_min,
-    maxTemperature: currentResponse.main.temp_max,
-    pressure: currentResponse.main.pressure,
-    humidity: currentResponse.main.humidity,
-    windSpeed: currentResponse.wind.speed,
-    sunrise: new Date(currentResponse.sys.sunrise),
-    sunset: new Date(currentResponse.sys.sunset),
-  };
-
-  const forecast = forecastResponse.daily
+  const forecast = response.daily
     .map((data) => {
       return {
         description: data.weather[0].description,
@@ -69,15 +51,44 @@ export const getWeatherData = async (query) => {
     })
     .slice(-7); // If we got 8 elements instead of 7, remove the first one (which is a forecast for today)
 
-  const historical = historicalResponse.map((data) => {
-    return {
-      description: data.current.weather[0].description,
-      icon: data.current.weather[0].icon,
-      temperature: data.current.temp,
-    };
-  });
+  return forecast;
+};
 
-  const res = { current, historical, forecast };
+const getPastData = async (dt, lat, lon) => {
+  const dateNow = new Date(dt * 1000);
+  const oldTimestamps = [];
+  for (let i = 0; i < 5; i++) {
+    const pastDate = new Date(dateNow.getTime());
+    pastDate.setDate(dateNow.getDate() - (i + 1));
 
-  return res;
+    oldTimestamps.push(pastDate.getTime() / 1000);
+  }
+
+  const response = await Promise.all(
+    oldTimestamps.map((timestamp) =>
+      callService(
+        `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${timestamp}`
+      )
+    )
+  );
+
+  const historical = response.map((data) => ({
+    description: data.current.weather[0].description,
+    icon: data.current.weather[0].icon,
+    temperature: data.current.temp,
+  }));
+
+  return historical;
+};
+
+const callService = async (url) => {
+  const response = await fetch(`${url}&units=metric&appid=${API_KEY}`);
+
+  if (!response.ok) {
+    console.log('error');
+  }
+
+  const data = response.json();
+
+  return data;
 };
