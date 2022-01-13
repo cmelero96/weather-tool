72b00e0f4be6986a94c1b9b45b7638e2973286bc
carlos.melero <cmelero96@gmail.com>
2022-01-13 10:35:26 +0100

    Added toggable extra info

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -8,15 +8,29 @@
     <div class="temp">Temp: {{ weather.temperature }} ºC</div>
     <div class="temp-min">Min: {{ weather.minTemperature }} ºC</div>
     <div class="temp-max">Max: {{ weather.maxTemperature }} ºC</div>
-    <div class="pressure">Pressure: {{ weather.pressure }} hPa</div>
-    <div class="humidity">Humidity: {{ weather.humidity }}%</div>
-    <div class="wind-speed">Wind speed: {{ weather.windSpeed }}m/s</div>
-    <div class="sunrise">Sunrise: {{ weather.sunrise }}</div>
-    <div class="sunset">Sunset: {{ weather.sunset }}</div>
+    <section class="extra-info-wrapper">
+      <header class="extra-info-title">
+        <strong>Additional information </strong>
+        <img
+          @click="toggleExtraInfo"
+          alt="toggle the additional information"
+          src="https://img.icons8.com/small/16/000000/expand-arrow.png"
+          :class="`caret ${showExtraInfo && 'rotated'}`"
+        />
+      </header>
+      <div v-if="showExtraInfo" class="extra-info-container">
+        <div class="pressure">Pressure: {{ weather.pressure }} hPa</div>
+        <div class="humidity">Humidity: {{ weather.humidity }}%</div>
+        <div class="wind-speed">Wind speed: {{ weather.windSpeed }}m/s</div>
+        <div class="sunrise">Sunrise: {{ weather.sunrise }}</div>
+        <div class="sunset">Sunset: {{ weather.sunset }}</div>
+      </div>
+    </section>
   </section>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
   props: {
     city: {
@@ -36,7 +50,10 @@ export default {
               https://openweathermap.org/img/wn/${icon}@4x.png`;
     };
 
-    return { getMainSource, getSources };
+    const showExtraInfo = ref(false);
+    const toggleExtraInfo = () => (showExtraInfo.value = !showExtraInfo.value);
+
+    return { showExtraInfo, toggleExtraInfo, getMainSource, getSources };
   },
 };
 </script>
@@ -44,5 +61,13 @@ export default {
 <style>
 .description {
   text-transform: uppercase;
+}
+
+.caret {
+  transition: all 0.15s ease;
+}
+
+.caret.rotated {
+  transform: rotateZ(180deg);
 }
 </style>
