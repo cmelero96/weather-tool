32864f1f327a4f686dd2042b1fc94b54d387ea66
carlos.melero <cmelero96@gmail.com>
2022-01-13 17:27:39 +0100

    Fixed bug

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,3 +17,24 @@ export const createButton = (title, tooltip, onClick) => {
 
   return button;
 };
+
+const WEEKDAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+export const getWeekdays = (dateNow, amountOfDays) => {
+  let dayNumber = dateNow.getDay();
+  const weekdays = [];
+
+  // Get sign of amountOfDays; default to +1 if amountOfDays = 0 for some reason
+  const direction = Math.sign(amountOfDays) || 1;
+
+  for (let count = 0; count < Math.abs(amountOfDays); count++) {
+    if (dayNumber >= WEEKDAYS.length) dayNumber = 0; // Correct when increasing
+    if (dayNumber < 0) dayNumber = WEEKDAYS.length - 1; // Correct when decreasing
+
+    console.log({ [count]: WEEKDAYS[dayNumber] });
+    weekdays.push(WEEKDAYS[dayNumber]);
+    dayNumber += 1 * direction; // Iterate forwards or backwards depending on sign
+  }
+
+  return weekdays;
+};
