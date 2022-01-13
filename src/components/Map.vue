f78c5ebfcdd8924df8601d8aeda8186e93556e58
carlos.melero <cmelero96@gmail.com>
2022-01-13 11:29:51 +0100

    Created custom button

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="map-wrapper">
-    <GMapMap
-      v-if="coord && coord.lat && coord.lon"
-      :center="{ lat: coord.lat, lng: coord.lon }"
-      :options="{}"
-      :zoom="12.5"
-      style="width: 100%; height: 100%"
-    >
-      <GMapCluster :zoomOnClick="true">
+    <GMapMap v-if="coord" :center="center" :options="{}" :zoom="12.5" ref="map">
+      <GMapCluster>
         <GMapMarker
+          v-for="(marker, index) in markers"
           :key="index"
-          v-for="(m, index) in markers"
-          :position="m.position"
-          :clickable="true"
-          :draggable="true"
-          @click="center = m.position"
+          :position="marker.position"
+          :clickable="false"
+          :draggable="false"
+          @click="center = marker.position"
         />
       </GMapCluster>
     </GMapMap>
@@ -22,11 +16,54 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { computed, ref, watch } from 'vue';
+
+const createButton = (map) => {
+  const controlUI = document.createElement('button');
+  controlUI.title = 'Click to zoom the map';
+  const controlText = document.createElement('div');
+  controlText.innerHTML = `Center`;
+  controlUI.appendChild(controlText);
+
+  controlUI.addEventListener('click', () => {
+    map.setZoom(map.getZoom() + 1);
+  });
+
+  return controlUI;
+};
 
 export default {
   props: {
     coord: { type: Object, required: true },
+  },
+  setup(props) {
+    const center = computed(() => ({
+      lat: props.coord.lat,
+      lng: props.coord.lon,
+    }));
+    const markers = ref([]);
+
+    watch(
+      () => props.coord,
+      (newCoord) => {
+        console.log(newCoord);
+        markers.value.push({ position: { lat: newCoord.lat, lng: newCoord.lon } });
+      }
+    );
+
+    const map = ref();
+    const button = ref();
+
+    watch(map, (googleMap) => {
+      if (googleMap) {
+        googleMap.$mapPromise.then((map) => {
+          const button = createButton(map);
+          map.controls[google.maps.ControlPosition.TOP_CENTER].push(button);
+        });
+      }
+    });
+
+    return { center, markers, map, button };
   },
 };
 </script>
